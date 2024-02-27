bf98eb32a6fd181881f08ac5ce8966205b164921
Xx <878323885@qq.com>
2024-02-27 07:18:33 +0000

    完成
    Signed-off-by: Xx <878323885@qq.com>

---
+++ vite.config.js
@@ -0,0 +1,58 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import vue from "@vitejs/plugin-vue";
+import { VueHooksPlusResolver } from '@vue-hooks-plus/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
+      dts: 'src/auto-imports.d.ts',
+      resolvers: [VueHooksPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  // 强制预构建插件包
+  optimizeDeps: {
+    // include: ["axios"],
+  },
+  // 打包配置
+  build: {
+    target: "modules",
+    outDir: "dist",
+    assetsDir: "assets",
+    minify: "terser",
+  },
+  // 本地运行配置，及反向代理配置
+  server: {
+    host: "127.0.0.1",
+    port: 9000,
+    hotOnly: false,
+    strictPort: false,
+    cors: true,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000", //代理接口
+        changeOrigin: true,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
